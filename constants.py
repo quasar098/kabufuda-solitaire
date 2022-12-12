66d0f34bfedfaef5ce3ad0fa1b9778557c1aaef6
quasar098 <patellmlg@gmail.com>
2022-12-12 13:44:37 -0500

    fix framerate + fix file

--- constants.py
+++ constants.py
@@ -1,2 +1,7 @@
 WIDTH, HEIGHT = 1072, 667+83
-FRAMERATE = 75
+try:
+    import win32api
+    FRAMERATE = win32api.EnumDisplaySettings(win32api.EnumDisplayDevices().DeviceName, -1).DisplayFrequency
+except ModuleNotFoundError:
+    print("win32api module not found! install it with pip install pywin32")
+    FRAMERATE = 60
